7f484abf32618267219aafa4168c387a47f18108
Anish Reddy <anishreddy20.ar@gmail.com>
2021-09-22 16:06:38 +1200

    add login screen, teal theme

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,10 +1,6 @@
-import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'package:uspeventsapp_v1/pages/Feedback_page.dart';
-import 'package:uspeventsapp_v1/pages/NewsFeed_page.dart';
 import 'package:uspeventsapp_v1/pages/main_drawer.dart';
-import './Profile_page.dart';
 import './main_drawer.dart';
 
 class MenuPage extends StatelessWidget {
